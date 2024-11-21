2188421d41e51b4c0a5c34016950260bc8386af5
Tumbichi <germanviglietti@gmail.com>
2024-11-21 17:05:06 -0300

    test(payment/webhook): log hmac verify status

--- src/app/api/payment/webhook/route.ts
+++ src/app/api/payment/webhook/route.ts
@@ -8,20 +8,32 @@ function verifySignature(
   xRequestId: string,
   paymentId: string
 ): true | never {
+  console.log('x-signature', xSignature);
   const xSignatureArr = xSignature.split(',');
 
   const ts = xSignatureArr[0].replace('t=', '');
+  console.log('ts', ts);
+
   const signaturePaymentHash = xSignatureArr[1].replace('v1=', '');
 
   const template = `id:${paymentId};request-id:${xRequestId};ts:${ts};`;
 
-  const hmacSHA256Signature = crypto
-    .createHmac('sha256', String(process.env.MERCADOPAGO_PAYMENT_SECRET_KEY))
-    .update(template)
-    .digest('hex');
+  const hmac = crypto.createHmac(
+    'sha256',
+    process.env.MERCADOPAGO_PAYMENT_SECRET_KEY as string
+  );
+  hmac.update(template);
 
-  console.log('hmacSHA256Signature', hmacSHA256Signature);
-  console.log('signaturePaymentSecretKey', signaturePaymentHash);
+  const sha256Signature = hmac.digest('hex');
+
+  console.log('sha256Signature', sha256Signature);
+  console.log('signaturePaymentHash', signaturePaymentHash);
+
+  if (sha256Signature === signaturePaymentHash) {
+    console.log('HMAC verified');
+  } else {
+    console.log('HMAC NOT verified');
+  }
 
   /* if (hmacSHA256Signature !== signaturePaymentHash) {
     throw new Error('Invalid payment signature');
