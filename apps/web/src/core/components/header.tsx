ea55e7f1e0d553c63a89ccbdb6abdcbd4d83297e
Tumbichi <germanviglietti@gmail.com>
2025-07-28 03:07:43 -0300

    fix(header): margins

--- apps/web/src/core/components/header.tsx
+++ apps/web/src/core/components/header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
         <Link href="/" className="mr-6">
           <p className="text-2xl font-medium">Belu Mont</p>
         </Link>
-        <div className="flex gap-2 ml-auto">
+        <div className="flex gap-2 ml-auto sm:gap-4">
           <Link
             href="/"
             className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-3 py-1 text-md lg:text-lg font-normal transition-colors hover:bg-black/5 hover:text-gray-900 hover:scale-125 hover:font-semibold focus:bg-black/20 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
           </Link>
           <Link
             href="/recetarios"
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-md lg:text-lg font-normal transition-colors hover:bg-black/5 hover:text-gray-900 hover:scale-105 hover:font-semibold focus:bg-black/20 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-3 py-2 text-md lg:text-lg font-normal transition-colors hover:bg-black/5 hover:text-gray-900 hover:scale-105 hover:font-semibold focus:bg-black/20 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           >
             {t('PRODUCTS')}
           </Link>
